1f5ea556036fc934d526622ff45bf0c5bb987deb
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-21 22:32:31 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -5,11 +5,10 @@ def countdown(n)
   while count > 0
     puts "#{count} SECOND(S)!"
     count -= 1
-    sleep(1)
   end
   "HAPPY NEW YEAR!"
 end
 
-# def countdown_with_sleep(1)
-#   sleep(1)
-# end
+def countdown_with_sleep(countdown)
+  sleep(1)
+end
